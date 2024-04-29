30544338c04b20f6368cae6355c5b7f995882f3a
caitlinrsmith <catie12smith@gmail.com>
2024-04-29 15:32:03 -0500

    did 1 bonus mission

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,24 @@
-//TODO: Add Your Code Below
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+console.log(response);
+return response.json()
+} ).then(function(json){  
+    let header = document.querySelector("h1");
+    header.innerHTML += `: ${json.length}`;
+ for(i = 0; i < json.length; i++){    
+    document.getElementById("container");
+    container.innerHTML += `
+    <div class="astronaut">
+    <div class="bio">
+        <h3>${json[i].firstName} ${json[i].lastName}</h3>
+        <ul>
+        <li>Hours in space: ${json[i].hoursInSpace}</li>
+        <li>Active: ${json[i].active}</li>
+        <li>Skills: ${json[i].skills}</li>
+        </ul>
+    </div>
+    <img class="avatar" src= "${json[i].picture}">
+</div>`;
+ }
+});
+     });
